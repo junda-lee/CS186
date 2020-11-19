d14f9e497e1c72efe89975e113949937fc2625f2
Jonty <1195576334@qq.com>
2020-11-19 10:37:04 +0800

    feat: implement the HeapFileIterator

--- src/main/java/simpledb/HeapFile.java
+++ src/main/java/simpledb/HeapFile.java
@@ -1,9 +1,7 @@
 package simpledb;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import java.io.*;
-import java.security.cert.TrustAnchor;
 import java.util.*;
 
 /**
@@ -21,6 +19,7 @@ public class HeapFile implements DbFile {
 
     private final TupleDesc td;
     private final File file;
+
 
     /**
      * Constructs a heap file backed by the specified file.
@@ -72,6 +71,7 @@ public class HeapFile implements DbFile {
     }
 
     // see DbFile.java for javadocs
+    @Override
     public Page readPage(PageId pid) {
         // some code goes here
         int tableId = pid.getTableId();
@@ -116,22 +116,92 @@ public class HeapFile implements DbFile {
             throws DbException, IOException, TransactionAbortedException {
         // some code goes here
         return null;
-        // not necessary for proj1
     }
 
     // see DbFile.java for javadocs
+    @Override
     public Page deleteTuple(TransactionId tid, Tuple t) throws DbException,
             TransactionAbortedException {
         // some code goes here
         return null;
-        // not necessary for proj1
     }
 
     // see DbFile.java for javadocs
+    @Override
     public DbFileIterator iterator(TransactionId tid) {
         // some code goes here
         return null;
     }
 
+    public static final class HeapFileIterator implements  DbFileIterator{
+
+        private final HeapFile heapFile;
+
+        private final TransactionId tid;
+
+        private Iterator<Tuple> it;
+
+        private int whichPage;
+
+        public HeapFileIterator(HeapFile heapFile, TransactionId tid) {
+            this.heapFile = heapFile;
+            this.tid = tid;
+        }
+
+        @Override
+        public void open() throws DbException, TransactionAbortedException {
+            whichPage = 0;
+            it = getPageTuple(whichPage);
+        }
+
+        public Iterator<Tuple> getPageTuple(int pageNum) throws DbException, TransactionAbortedException {
+            if (pageNum < 0 || pageNum >= heapFile.numPages()) {
+                throw new DbException(String.format("heapfile %d does not contain page %d!", pageNum,heapFile.getId()));
+            }
+            HeapPageId pid = new HeapPageId(heapFile.getId(), pageNum);
+            HeapPage page = (HeapPage)Database.getBufferPool().getPage(tid, pid, Permissions.READ_ONLY);
+            return page.iterator();
+        }
+
+        @Override
+        public boolean hasNext() throws DbException, TransactionAbortedException {
+            if (it == null){
+                return false;
+            }
+            if (!it.hasNext()) {
+                if (whichPage < heapFile.numPages() - 1){
+                    it = getPageTuple(++whichPage);
+                    return it.hasNext();
+                }else {
+                    return false;
+                }
+            }else {
+                return true;
+            }
+        }
+
+        @Override
+        public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+            // TODO Auto-generated method stub
+            if(it == null || !it.hasNext()){
+                throw new NoSuchElementException();
+            }
+            return it.next();
+
+        }
+
+        @Override
+        public void rewind() throws DbException, TransactionAbortedException {
+            // TODO Auto-generated method stub
+            close();
+            open();
+        }
+
+        @Override
+        public void close() {
+            // TODO Auto-generated method stub
+            it = null;
+        }
+    }
 }
 
